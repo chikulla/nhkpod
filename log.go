023db60cd471922adc2b8e51534484088140e93a
chikulla <chcklla+git@gmail.com>
2022-04-23 15:59:30 -0700

    clear logs for initial setup

--- log.go
+++ log.go
@@ -7,6 +7,10 @@ import (
 )
 
 func SetupLogOutput(e Env) error {
+	err := os.Remove(e.LogFile)
+	if err != nil {
+		return err
+	}
 	file, err := os.OpenFile(e.LogFile, os.O_CREATE|os.O_RDWR, 0666)
 	if err != nil {
 		return err
